786d2d77b41b8d04aa0a79000b996cf11981fa27
Mohammed Al-Asfar <elasfarc@gmail.com>
2021-09-21 14:49:38 +0200

    Add animals data

--- data.sql
+++ data.sql
@@ -1,5 +1,10 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+INSERT INTO ANIMALS(name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Agumon', DATE '2020-02-03', 10.23, TRUE, 0 );
+
+INSERT INTO ANIMALS(name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Gabumon', DATE '2018-11-15', 8, TRUE, 2 );
+
+INSERT INTO ANIMALS(name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Pikachu', DATE '2021-01-07', 15.04, FALSE, 1 );
+
+INSERT INTO ANIMALS(name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Devimon', DATE '2017-05-12', 11, TRUE, 5 );
+
