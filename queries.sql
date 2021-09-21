84c1fc261958a40e776250ec4491c8aa60e87a37
Mohammed Al-Asfar <elasfarc@gmail.com>
2021-09-21 15:48:45 +0200

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,7 +1,7 @@
 /*Queries that provide answers to the questions from all projects.*/
 
 
-SELECT * FROM animals WHERE name LIKE 'mon%';
+SELECT * FROM animals WHERE name LIKE '%mon';
 
 SELECT name FROM animals 
 WHERE EXTRACT(YEAR FROM date_of_birth) BETWEEN 2016 AND 2019;
@@ -12,9 +12,9 @@ WHERE neutered=TRUE AND escape_attempts<3;
 SELECT date_of_birth FROM animals 
 WHERE name='Agumon' OR  name='Pikachu';
 
-SELECT name, escape_attempts                                                                        FROM animals                                                                                                     WHERE weight_kg>10.5;
+SELECT name, escape_attempts FROM animals WHERE weight_kg>10.5;
 
-SELECT * FROM animals                                                                                                                                                                WHERE neutered=TRUE;
+SELECT * FROM animals WHERE neutered=TRUE;
 
 SELECT * from animals
 WHERE NOT name='Gabumon';
