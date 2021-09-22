0c75130d1cfb192d2ec410f9c250c70956ffede6
Mohammed Al-Asfar <elasfarc@gmail.com>
2021-09-22 18:45:54 +0200

    Add Animals data

--- data.sql
+++ data.sql
@@ -8,3 +8,14 @@ INSERT INTO ANIMALS(name, date_of_birth, weight_kg, neutered, escape_attempts) V
 
 INSERT INTO ANIMALS(name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Devimon', DATE '2017-05-12', 11, TRUE, 5 );
 
+INSERT INTO ANIMALS(name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Charmander', DATE '2020-02-08', 11, FALSE, 0 );
+
+INSERT INTO ANIMALS(name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Plantmon', DATE '2022-11-15', 5.7, TRUE, 2 );
+
+INSERT INTO ANIMALS(name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Squirtle', DATE '1993-04-02', 12.13, FALSE, 3 );
+
+INSERT INTO ANIMALS(name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Angemon', DATE '2005-06-12', 45, TRUE, 1 );
+
+INSERT INTO ANIMALS(name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Boarmon', DATE '2005-06-07', 20.4, TRUE, 7 );
+
+INSERT INTO ANIMALS(name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Blossom', DATE '1998-10-13', 17, TRUE, 3 );
